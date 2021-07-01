fff3994ee2fc5ddf855001cacd168dcdff00e9e0
Alex Sousa <alex.sousa20@hotmail.com>
2021-07-01 10:54:53 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -0,0 +1,30 @@
+package one.digitalinnovation.personapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("Api de Pessoas") String description){
+		return new OpenAPI()
+				.info(new Info()
+				.title(description)
+				.version("1.0")
+				.termsOfService("http://suagger.io/terms")
+				.license(new License().name("Apache 2.0")
+				.url("http://springdoc.org")));
+	}
+
+}
